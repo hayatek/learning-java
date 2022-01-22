9600f179813dff1647dc558b463ff30cda3edac3
hayatek <stream.hyt@gmail.com>
2022-01-23 06:24:05 +0900

    add JavaBasic

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
-    public static void main(String[] args){
+    public static void main(String[] hoge){
         System.out.println("Hello World!!");
+        //Hello, this is a commnet line
     }
 }
---
+++ JavaBasics.java
@@ -0,0 +1,11 @@
+public class JavaBasics {
+    public static void main(String[] args){
+        // byte -128〜127
+        byte numberTest = -128;
+        // short -32768〜32767
+        short numberTest = -32768;
+        // char 0〜65535
+        char numberTest = 'も';
+        System.out.println(numberTest);
+    }
+}
